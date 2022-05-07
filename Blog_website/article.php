3f781f79e4f4413b1b45f942fc036146597b649d
PranavTiwari44 <tiwa0027@algonquinlive.com>
2022-05-07 00:31:43 -0400

    Organized blog website code, added some security features like .htaccess file and basic error checking!

---
+++ Blog_website/article.php
@@ -0,0 +1,30 @@
+<?php
+
+require 'includes/database.php';
+
+if (isset($_GET['id']) and is_numeric($_GET['id'])) {
+  $sql = "Select * from article where id = " . $_GET['id'];
+
+  $results = mysqli_query($conn, $sql);
+
+  if ($results === false) {
+    echo mysqli_error($conn);
+  } else {
+    $article = mysqli_fetch_assoc($results);
+  }
+} else {
+  $article = null;
+}
+?>
+<?php require 'includes/header.php'; ?>
+  <?php if ($article === null) : ?>
+    <p>Article not found.</p>
+  <?php else : ?>
+
+    <article>
+      <h2><?= $article['title']; ?></h2>
+      <p><?= $article['content']; ?></p>
+    </article>
+
+  <?php endif; ?>
+<?php require 'includes/footer.php'; ?>
